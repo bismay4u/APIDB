d224581354909acb01439d0ec1627be6745da82b
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2025-07-11 01:43:41 +0530

    Update connections_sample.js

--- config/connections_sample.js
+++ config/connections_sample.js
@@ -1,3 +1,34 @@
 {
-    
+    "global": {
+        "apikey": "",
+        "secret": ""
+    },
+    "apidbkey1": {
+        "apikey": "",
+        "secret": "",
+        "driver": "mysql",
+        "uri": "",
+        "host": "",
+        "port": "",
+        "database": "db",
+        "user": "user1",
+        "pwd": "pass",
+        "prefix": "",
+        "suffix": "",
+        "block": [],
+        "readonly": false,
+        "allowSQL": true,
+        "params": {
+            
+        }
+    },
+    "apidbkey2": {
+        "driver": "mysql",
+        "uri": "",
+        "prefix": "",
+        "suffix": "",
+        "block": [],
+        "readonly": false,
+        "allowSQL": true
+    }
 }
