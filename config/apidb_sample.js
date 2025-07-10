0dd47f62fe88baa2b84a29553fb4122b0d994e2d
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2025-07-11 01:47:09 +0530

    Update apidb_sample.js

--- config/apidb_sample.js
+++ config/apidb_sample.js
@@ -1,17 +1,20 @@
 const packageConfig = require('../package.json');
 
 module.exports = {
-	name: packageConfig.title,
-	version: packageConfig.version,
-	packageid: packageConfig.name,
-	env: process.env.NODE_ENV || 'development',
-	port: process.env.PORT || 8888,
-	base_url: process.env.BASE_URL || 'http://localhost:8888',
+    name: packageConfig.title,
+    version: packageConfig.version,
+    packageid: packageConfig.name,
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8888,
+    base_url: process.env.BASE_URL || 'http://localhost:8888',
 
-	debug: true,
-	allow_home_info: false,
+    debug: true,
 
-	LOGGER: [
+    "WHITELISTED_IP": {
+    
+    },
+
+    LOGGER: [
         {
             type: 'rotating-file',
             period: '1d',                       // daily rotation
@@ -23,47 +26,36 @@ module.exports = {
             level: 'error',                     //log level
             stream: process.stdout              // log INFO and above to stdout
         }
-	],
-	
-	DB_SPECIAL_COLUMNS: {
-		"CREATED_ON":"created_on",
-		"UPDATED_ON":"edited_on",
-		"DELETED_ON":"deleted_on",
+    ],
+    
+    DB_SPECIAL_COLUMNS: {
+        "CREATED_ON":"created_on",
+        "UPDATED_ON":"edited_on",
+        "DELETED_ON":"deleted_on",
 
-		"CREATED_BY":"created_by",
-		"UPDATED_BY":"edited_by",
-		"DELETED_BY":"deleted_by",
+        "CREATED_BY":"created_by",
+        "UPDATED_BY":"edited_by",
+        "DELETED_BY":"deleted_by",
 
-		"SOFT_DELETE":"blocked"
-	},
+        "SOFT_DELETE":"blocked"
+    },
 
-	cache : {
-		host: '127.0.0.1',   // Redis host
-		// host: '192.168.0.1',   // Redis host
-		port: 6379,          // Redis port
-		family: 4,           // 4 (IPv4) or 6 (IPv6)
-		//password: 'auth',
-		db: 0
-	},
+    cache : {
+        host: '127.0.0.1',   // Redis host
+        // host: '192.168.0.1',   // Redis host
+        port: 6379,          // Redis port
+        family: 4,           // 4 (IPv4) or 6 (IPv6)
+        //password: 'auth',
+        db: 0
+    },
 
-	//unused variables
-
-	noauth: [
-		"/auth",
-		"/ping",
-		"/test",
-		"/"
-	],
-	jwt: {
-		secret: "Zv7QAeJWhWz6j2kA"
-	},
-	mail:{
-		host: 'email-smtp.us-east-1.amazonaws.com',
-		port: 587,
-		secure: false, // true for 465, false for other ports
-		auth: {
-		    user: "smtp-userid", // generated ses user
-		    pass: "smtp-pwd" // generated ses password
-		}
-	}
+    mail:{
+        host: 'email-smtp.us-east-1.amazonaws.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "smtp-userid", // generated ses user
+            pass: "smtp-pwd" // generated ses password
+        }
+    }
 };
